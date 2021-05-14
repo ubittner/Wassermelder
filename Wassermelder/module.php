c5112b6ecc22c3c8f950cd24977792cdba3dd085
ubittner <ulrich.bittner@me.com>
2021-05-14 10:11:13 +0200

    updates @ 1.00-3 (20210514)

--- Wassermelder/module.php
+++ Wassermelder/module.php
@@ -444,6 +444,32 @@ class Wassermelder extends IPSModule
                 'MessageDescription' => $messageDescription,
                 'rowColor'           => $rowColor];
         }
+        // Status
+        $formData['status'][0] = [
+            'code'    => 101,
+            'icon'    => 'active',
+            'caption' => 'Wassermelder wird erstellt',
+        ];
+        $formData['status'][1] = [
+            'code'    => 102,
+            'icon'    => 'active',
+            'caption' => 'Wassermelder ist aktiv (ID ' . $this->InstanceID . ')',
+        ];
+        $formData['status'][2] = [
+            'code'    => 103,
+            'icon'    => 'active',
+            'caption' => 'Wassermelder wird gelöscht (ID ' . $this->InstanceID . ')',
+        ];
+        $formData['status'][3] = [
+            'code'    => 104,
+            'icon'    => 'inactive',
+            'caption' => 'Wassermelder ist inaktiv (ID ' . $this->InstanceID . ')',
+        ];
+        $formData['status'][4] = [
+            'code'    => 200,
+            'icon'    => 'inactive',
+            'caption' => 'Es ist Fehler aufgetreten, weitere Informationen unter Meldungen, im Log oder Debug! (ID ' . $this->InstanceID . ')',
+        ];
         return json_encode($formData);
     }
 
